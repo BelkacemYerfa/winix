c023684cb0b65f4f01c2cc11b6199d4670355e9d
BelkacemYerfa <belkacem.yerfa@gmail.com>
2024-07-27 16:33:33 +0100

    add : print the $ sign

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+     //Uncomment this block to pass the first stage
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
     let stdin = io::stdin();
